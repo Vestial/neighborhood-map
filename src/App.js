2216ca096b30b016bbbbbf38f98363815ed218b6
Vestial <penskewilliano@gmail.com>
2018-08-18 15:31:57 +0200

    Add sushi restaurant locations on the map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,122 @@
-import React, { Component } from 'react';
-import './App.css';
-import Map from './components/Map'
+import React, { Component } from "react";
+import "./App.css";
+import SushiMap from "./components/SushiMap";
+import sortBy from "sort-by";
 
 class App extends Component {
+  markers = [];
+
+  state = {
+    // Default map's values
+    center: { lat: 52.3774769, lng: 9.7407584 },
+    zoom: 14,
+
+    // Parameters to set active marker and show InfoWindow on map component
+    activeMarker: {},
+    selectedPlace: {},
+    showingInfoWindow: false,
+
+    // Array with all fetched sushis
+    sushiList: [],
+    foundSushis: []
+  };
+
+  componentDidMount() {
+    this.getSushiList();
+  }
+
+  getSushiList() {
+    fetch(
+      "https://api.foursquare.com/v2/venues/search?near=Hanover&query=sushi&category=4bf58dd8d48988d111941735&limit=100&radius=6000&intent=browse&client_id=QEALSM14NUIOQ3IYJR5GOA3BVXC1LEODK3DK2RIEXFWILEEX&client_secret=NHD5ISSOZSKMGBOMAQN1024SR4HW4AGADQ4KVIJARTJVHBPV&v=20180101&locale=en"
+    )
+      .then(res => res.json())
+      .then(data => {
+        const sushiList = data.response.venues;
+        if (sushiList.length === 0) {
+          window.alert("No places found. Please try to refresh the page.");
+        }
+        sushiList.sort(sortBy("name"));
+        sushiList.forEach(sushi => {
+          if (!sushi.location.address) {
+            sushi.location.address = "No address available";
+            sushi.location.formattedAddress.unshift("No address available");
+          }
+        });
+        const foundSushis = sushiList;
+        this.setState({ sushiList, foundSushis });
+        console.log(sushiList);
+      });
+  }
+
+  onMarkerCreated = marker => {
+    if (marker !== null) {
+      this.markers.push(marker);
+    }
+  };
+
+  clickMarker = (props, marker) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+    this.zoomPlace(props.position.lat, props.position.lng);
+  };
+
+  zoomPlace = (focusLat, focusLng) => {
+    let center = { lat: focusLat + 0.002, lng: focusLng };
+    let zoom = 15;
+    this.setState({ center, zoom });
+  };
+
+  getCenterAndZoom() {
+    let objectsBounds = new window.google.maps.LatLngBounds();
+    let cenLat;
+    let cenLng;
+    let center;
+    let zoom;
+    if (this.state.foundSushis.length === 1) {
+      cenLat = this.state.foundSushis[0].location.lat;
+      cenLng = this.state.foundSushis[0].location.lng;
+      center = { lat: cenLat, lng: cenLng };
+      zoom = 15;
+    } else if (this.state.foundSushis.length === 0) {
+      center = { lat: 52.3774769, lng: 9.7407584 };
+      zoom = 12;
+    } else {
+      Object.values(this.state.foundSushis).map(marker => {
+        let lat = parseFloat(marker.location.lat);
+        let long = parseFloat(marker.location.lng);
+        let point = new window.google.maps.LatLng(lat, long);
+        objectsBounds.extend(point);
+        return objectsBounds;
+      });
+      cenLat = (objectsBounds.f.b + objectsBounds.f.f) / 2;
+      cenLng = (objectsBounds.b.b + objectsBounds.b.f) / 2;
+      center = { lat: cenLat, lng: cenLng };
+      zoom = 14;
+    }
+    this.setState({ center, zoom });
+  }
+
   render() {
     return (
       <div className="App">
-      <Map />
+        <SushiMap
+          center={this.state.center}
+          zoom={this.state.zoom}
+          sushis={this.state.foundSushis}
+          selectedPlace={this.state.selectedPlace}
+          onMarker={this.state.activeMarker}
+          onVisible={this.state.showingInfoWindow}
+          onToggle={this.toggleAside}
+          onMarkerCreated={this.onMarkerCreated}
+          onMarkerClick={this.clickMarker}
+          onInfoWindowClose={this.closeInfoWindow}
+          onMapClicked={this.clickMap}
+          onFocus={this.zoomPlace}
+          onDezoom={this.getFar}
+        />
       </div>
     );
   }
---
+++ src/components/SushiMap.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import sushi from "../assets/sushi.png";
+
+export class SushiMap extends Component {
+  render() {
+    return (
+      <section className="map">
+        <Map
+          google={this.props.google}
+          onClick={this.props.onMapClicked}
+          zoom={this.props.zoom}
+          center={this.props.center}
+          initialCenter={this.props.center}
+          initialZoom={this.props.zoom}
+        >
+          {this.props.sushis.map(sushi => (
+            <Marker
+              id={sushi.id}
+              key={sushi.id}
+              name={sushi.name}
+              onClick={(props, marker) =>
+                this.props.onMarkerClick(props, marker)
+              }
+              street={sushi.location.formattedAddress[0]}
+              city={sushi.location.formattedAddress[1]}
+              position={{ lat: sushi.location.lat, lng: sushi.location.lng }}
+              ref={this.props.onMarkerCreated}
+              animation={
+                this.props.selectedPlace.name === sushi.name &&
+                this.props.google.maps.Animation.BOUNCE
+              }
+            />
+          ))}
+
+          <InfoWindow
+            marker={this.props.onMarker}
+            onClose={this.props.onInfoWindowClose}
+            visible={this.props.onVisible}
+          >
+            <Card style={{ maxWidth: "250px" }}>
+              <CardContent style={{ padding: "0" }}>
+                <Typography
+                  variant="title"
+                  component="h4"
+                  style={{ maxWidth: "200px" }}
+                >
+                  {this.props.selectedPlace.name}
+                </Typography>
+                <img
+                  src={sushi}
+                  style={{ maxWidth: "40px", margin: "10px 0" }}
+                  alt="Sushi logo"
+                  className="logo"
+                />
+                <Typography component="p" style={{ maxWidth: "200px" }}>
+                  {this.props.selectedPlace.street}
+                  <br />
+                  {this.props.selectedPlace.city}
+                </Typography>
+                <Button
+                  size="small"
+                  tabIndex="0"
+                  role="button"
+                  href={`https://de.foursquare.com/explore?mode=url&near=Hannover&nearGeoId=72057594040838767&q=${
+                    this.props.selectedPlace.name
+                  }`}
+                  target="_blank"
+                  style={{ margin: "10px 0" }}
+                  variant="contained"
+                  color="secondary"
+                  disableFocusRipple={false}
+                  disableRipple={false}
+                >
+                  more
+                </Button>
+              </CardContent>
+            </Card>
+          </InfoWindow>
+        </Map>
+      </section>
+    );
+  }
+}
+
+SushiMap.propTypes = {
+  google: PropTypes.object.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  onMarkerCreated: PropTypes.func.isRequired,
+  onMarker: PropTypes.object,
+  onInfoWindowClose: PropTypes.func.isRequired,
+  onVisible: PropTypes.bool.isRequired,
+  onMapClicked: PropTypes.func.isRequired,
+  center: PropTypes.object.isRequired,
+  zoom: PropTypes.number.isRequired,
+  sushis: PropTypes.array.isRequired,
+  selectedPlace: PropTypes.object.isRequired
+};
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDzLarewcl1NBAfRVykLSGnsNNFAoZoD-4"
+})(SushiMap);
